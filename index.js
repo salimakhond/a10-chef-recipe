49a932395309d74b014d4da775923ef549c35088
salimakhond <salimakhond@gmai.com>
2023-05-02 03:12:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+
+
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send('recipes is running')
+});
+
+
+
+app.listen(port, () => {
+    console.log(`recipes API is running on port: ${port}`)
+})
